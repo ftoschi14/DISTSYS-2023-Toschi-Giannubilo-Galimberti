4e34a0b90a446ee1540fcb6fc5c56bba87e3b967
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2024-03-05 14:46:10 +0100

    Implemented ChangeKey
    Tested and working partial implementation

--- modules/worker.cc
+++ modules/worker.cc
@@ -5,6 +5,7 @@
 #include <omnetpp.h>
 
 #include "setup_m.h"
+#include "datainsert_m.h"
 #include "BatchLoader.h"
 
 using namespace omnetpp;
@@ -12,16 +13,24 @@ using namespace omnetpp;
 class Worker : public cSimpleModule{
 private:
 	std::vector<int> data;
+	std::map<int, DataInsertMessage*> unstableMessages;
+	std::map<int, cMessage*> timeouts;
 	std::string fileName;
 	std::string fileProgressName;
 	int batchSize;
 	int workerId;
+	float timeout;
 	BatchLoader* loader;
 
 protected:
 	virtual void initialize() override;
 	virtual void handleMessage(cMessage *msg) override;
 	void handleSetupMessage(SetupMessage *msg);
+	void handleDataInsertMessage(DataInsertMessage *msg);
+	int changeKey(int data);
+	int reduce(int data[]);
+	void sendData(int newKey, int value);
+	int getWorkerGate(int destID);
 };
 
 Define_Module(Worker);
@@ -30,15 +39,59 @@ void Worker::initialize(){
 	// TODO
 	batchSize = par("batchSize").intValue();
 	data.resize(batchSize);
+	timeout=5;
 }
 
 void Worker::handleMessage(cMessage *msg){
+	/* Timeout Message chunk
+	*  Get the context pointer, if not null it should correspond to a request ID
+	*  Get the corresponding timeout and insertMsg, re-send it and re-schedule a timeout.
+	*/
+	int *pReqID = static_cast<int *>(msg->getContextPointer());
+	if(pReqID != nullptr){
+		int reqID = *pReqID;
+		delete pReqID;
+		// Check if the corresponding timeout exists
+		auto timeoutMsg = timeouts.find(reqID);
+		if(timeoutMsg != timeouts.end()){
+			delete timeoutMsg->second; // Safe because it's not scheduled anymore.
+
+			// Check if the corresponding insertMessage exists
+			auto insertMsg = unstableMessages.find(reqID);
+			if(insertMsg != unstableMessages.end()){
+
+				// Get the worker ID from the message
+				int destWorker = insertMsg->second->getDestWorker();
+				DataInsertMessage* insertMsgCopy = insertMsg->second->dup();
+				send(insertMsgCopy, "out", getWorkerGate(destWorker));
+
+				// Reschedule the timeout for this message
+                cMessage* newTimeoutMsg = new cMessage(("Timeout-" + std::to_string(reqID)).c_str());
+                newTimeoutMsg->setContextPointer(new int(reqID));
+                scheduleAt(simTime() + timeout, newTimeoutMsg);
+
+                // Update maps with the new timeout message and remove the old one
+                timeouts[reqID] = newTimeoutMsg;
+                return;
+			}
+			return;
+		}
+	}
+
 	// Setup Message chunk
 	SetupMessage *setupMsg = dynamic_cast<SetupMessage *>(msg);
-    if (setupMsg != nullptr) {
+    if(setupMsg != nullptr){
         // Successfully cast to SetupMessage, handle it
         handleSetupMessage(setupMsg);
         return;
+    }
+
+    // DataInsertMessage (Could be either to insert here, or an ACK)
+    DataInsertMessage *dataInsertMsg = dynamic_cast<DataInsertMessage *>(msg);
+    if(dataInsertMsg != nullptr){
+    	//Successfully cast to DataInsertMessage, handle it
+    	handleDataInsertMessage(dataInsertMsg);
+    	return;
     }
 
     // TODO Other messages...
@@ -61,7 +114,7 @@ void Worker::handleSetupMessage(SetupMessage *msg){
 	data_file.open(fileName);
 
 	for (int i = 0; i < dataSize; i++) {
-	    // Directly write to the file without using std::format
+	    // Directly write to the file
 	    data_file << workerId << ',' << msg->getData(i) << '\n';
 	}
 
@@ -73,4 +126,94 @@ void Worker::handleSetupMessage(SetupMessage *msg){
 
 	// Load first batch
 	data = loader->loadBatch();
-};
+}
+
+void Worker::handleDataInsertMessage(DataInsertMessage *msg){
+	// Check if it is an ACK or an insertion to me (workerID)
+	if(msg->getAck()){
+		// Look for corresponding reqID and cancel timeout
+		int reqID = msg->getReqID();
+
+		auto timeoutMsg = timeouts.find(reqID);
+		if(timeoutMsg != timeouts.end()){
+			EV << "Received ACK, cancelling timeout";
+			cancelAndDelete(timeoutMsg->second);
+			timeouts.erase(reqID);
+		}
+
+		auto insertMsg = unstableMessages.find(reqID);
+		if(insertMsg != unstableMessages.end()){
+			EV << "Received ACK, deleting unstable message";
+			unstableMessages.erase(reqID);
+		}
+		return;
+	} else {
+		// Insert new data point into data vector
+		DataInsertMessage* insertMsg = new DataInsertMessage();
+		insertMsg->setReqID(msg->getReqID());
+		insertMsg->setAck(true);
+
+		send(insertMsg, "out", msg->getArrivalGate()->getIndex());
+		delete msg;
+	}
+}
+
+int Worker::changeKey(int data){
+	// TODO crash probability
+	int res = bernoulli(0.05); // 5% of a data point changing key
+	if(res){
+		// Pick new random key, different from current worker
+		int newKey = -1;
+		while(newKey < 0 || newKey == workerId){
+			newKey = uniform(0, par("numWorkers").intValue());
+		}
+		return newKey;
+	}
+	return -1;
+}
+
+int Worker::reduce(int data[]){
+	// TODO crash probability
+	return 0;
+}
+
+void Worker::sendData(int newKey, int value){
+	// Create message
+	DataInsertMessage* insertMsg = new DataInsertMessage();
+
+	// Insert <k, v> pair
+	insertMsg->setDestWorker(newKey);
+	insertMsg->setValue(value);
+	
+	// Generate request ID
+	int reqID = uniform(INT_MIN, INT_MAX);
+	insertMsg->setReqID(reqID);
+
+	DataInsertMessage* insertMsgCopy = insertMsg->dup();
+
+	// Get correct gate and send duplicate insert message
+	int outputGate = getWorkerGate(newKey);
+	send(insertMsgCopy, "out", outputGate);
+
+	//add req to queue and wait for ACK, set timeout
+	unstableMessages[reqID] = insertMsg;
+
+	cMessage* timeoutMsg = new cMessage(("Timeout-" + std::to_string(reqID)).c_str());
+	timeoutMsg->setContextPointer(new int(reqID));
+
+	scheduleAt(simTime()+timeout, timeoutMsg);
+
+	timeouts[reqID] = timeoutMsg;
+}
+
+int Worker::getWorkerGate(int destID){
+	if(destID < workerId){
+		//out[newKey + 1] (Corresponding to worker "newKey")
+		return destID + 1;
+	} else if(destID > workerId) {
+		//out[newKey]
+		return destID;
+	}
+	EV << "Error: self getWorkerGate";
+	return -1;
+}
