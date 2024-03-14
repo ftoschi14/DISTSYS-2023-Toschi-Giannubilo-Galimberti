2c58b6ae1e3eb36e08f32c2a0e76bde44c58c1fb
giubbilo <92432998+giubbilo@users.noreply.github.com>
2024-03-14 16:40:47 +0100

    Updates for leader and finishSim message
    Added the finishSimulation message and updated the leader file

--- modules/leader.cc
+++ modules/leader.cc
@@ -2,54 +2,44 @@
 #include <string.h>
 #include <omnetpp.h>
 #include <filesystem>
-#include<cstdlib>
+#include <cstdlib>
+#include <ctime>
+
 #include "setup_m.h"
 #include "schedule_m.h"
 #include "finishLocalElaboration_m.h"
 #include "checkChangeKeyAck_m.h"
+#include "finishSim_m.h"
 
 namespace fs = std::filesystem;
 using namespace omnetpp;
 
 class Leader : public cSimpleModule
 {
-    public:
-        ~Leader()
-        {
-            removeWorkersDirectory();
-        }
     protected:
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
-        //void sendTestSetupMessage(int gateIndex);
         void createWorkersDirectory();
+        void removeWorkersDirectory();
         void sendData(int id_dest);
         void sendSchedule();
         void handleFinishElaborationMessage(cMessage *msg, bool local, int id);
     private:
+        bool finished;
         int numWorkers;
         std::vector<int> finishedWorkers;
-        bool finished;
-        void removeWorkersDirectory()
-        {
-            fs::path dirPath = "Data";
-            // Check if the directory exists before trying to remove it
-            if (fs::exists(dirPath) && fs::is_directory(dirPath))
-            {
-                // Remove the directory and its contents recursively
-                fs::remove_all(dirPath);
-            }
-        }
 };
 
 Define_Module(Leader);
 
 void Leader::initialize()
 {
-	//Create './Data/Worker_i' Directory for worker data
+    // Remove all previous folders in './Data/'
+    removeWorkersDirectory();
+
+	//Create './Data/Worker_i' directory for worker data
 	createWorkersDirectory();
     
-	//sendTestSetupMessage(0);
     numWorkers = par("numWorkers").intValue();
     for(int i = 0; i < numWorkers; i++)
     {
@@ -58,10 +48,40 @@ void Leader::initialize()
 
 	for(int i = 0; i < numWorkers; i++)
 	{
-        srand((unsigned) time(NULL)+i);
+	    srand((unsigned)time(NULL) + i);
+	    // Call the function for sending the data
 	    sendData(i);
 	}
+	// Call the function for sending the schedule
 	sendSchedule();
+}
+
+void Leader::createWorkersDirectory()
+{
+    fs::path dirPath = "Data";
+    if(!fs::exists(dirPath))
+    {
+        fs::create_directory(dirPath);
+    }
+
+    int numWorkers = par("numWorkers").intValue();
+    fs::path subPath;
+    for(int i = 0; i < numWorkers; i++)
+    {
+        subPath = "Worker_" + std::to_string(i);
+        fs::create_directory(dirPath/subPath);
+    }
+}
+
+void Leader::removeWorkersDirectory()
+{
+    fs::path dirPath = "Data";
+    // Check if the directory exists before trying to remove it
+    if(fs::exists(dirPath) && fs::is_directory(dirPath))
+    {
+        // Remove the directory and its contents recursively
+        fs::remove_all(dirPath);
+    }
 }
 
 void Leader::handleMessage(cMessage *msg)
@@ -78,13 +98,10 @@ void Leader::handleMessage(cMessage *msg)
         handleFinishElaborationMessage(finishLocalMsg, false, checkChangeKeyAckMsg -> getWorkerId());
         return;
     }
-    
-	
 }
 
 void Leader::handleFinishElaborationMessage(cMessage *msg, bool local, int id)
-{   
-
+{
     finishedWorkers[id] = 1;
     delete msg;
     finished = true;
@@ -100,75 +117,43 @@ void Leader::handleFinishElaborationMessage(cMessage *msg, bool local, int id)
     {
         for(int i = 0; i < numWorkers; i++)
         {
-            if(local){
+            if(local)
+            {
                 FinishLocalElaborationMessage* finishLocalMsg = new FinishLocalElaborationMessage();
                 finishLocalMsg -> setWorkerId(i);
-                EV<<"\nEVERYONE FINISHED ITS LOCAL ELABORATION\n";
+                EV << "Worker " << finishLocalMsg -> getWorkerId() << " has finished its local elaboration" << std::endl;
                 send(finishLocalMsg, "out", i);
                 for(int i = 0; i < numWorkers; i++)
                 {
                     finishedWorkers[i] = 0;
                 }
-            }else{
-                CheckChangeKeyAckMessage* checkChangeKeyAckMsg = new CheckChangeKeyAckMessage();
-                checkChangeKeyAckMsg -> setWorkerId(i);
-                EV<<"\nAPPLICATION FINISHED AT LEADER SIDE\n";
-                send(checkChangeKeyAckMsg, "out", i);
             }
+                else
+                {
+                    FinishSimMessage* finishSimMsg = new FinishSimMessage();
+                    finishSimMsg -> setWorkerId(i);
+                    EV << "Application terminated at Leader side\n";
+                    send(finishSimMsg, "out", i);
+                }
         }
     }
 }
 
-void Leader::createWorkersDirectory()
-{
-	fs::path dirPath = "Data";
-	if(!fs::exists(dirPath))
-	{
-		fs::create_directory(dirPath);
-	}
-
-	int numWorkers = par("numWorkers").intValue();
-	fs::path subPath;
-	for(int i = 0; i < numWorkers; i++)
-	{
-		subPath = "Worker_" + std::to_string(i);
-		fs::create_directory(dirPath/subPath);
-	}
-}
-
-/*
-void Leader::sendTestSetupMessage(int gateIndex){
-	SetupMessage *msg = new SetupMessage();
-	msg->setAssigned_id(0);
-
-	int numOfElements = 10;
-	msg->setDataArraySize(numOfElements);
-
-	for (int i = 0; i < numOfElements; ++i) {
-    	msg->setData(i, i*2); // Fill with some values
-	}
-	send(msg, "out", gateIndex);
-	return;
-}
-*/
-
 void Leader::sendData(int idDest)
 {
     SetupMessage *msg = new SetupMessage();
-
     msg -> setAssigned_id(idDest);
-    
-    // Genero un numero casuale che indica la dimensione del vettore da inviare
-    //int numElements = intuniform(1, 100);
-    int numElements = 30;
-    std::cout << "Num elements: " << numElements << "\n";
+
+    // Generate a random dimension for the array of values
+    int numElements = (rand () % 10) + 1;
+    std::cout << "#elements: " << numElements << std::endl;
 
     msg -> setDataArraySize(numElements);
     std::cout << "Array: ";
     for(int j = 0; j < numElements; j++)
     {
-        // Genero un valore casuale tra 1 e 100
-        int value = 1 + (rand() % 100);
+        // Generate a random int value starting from 1
+        int value = (rand() % 100) + 1;
         std::cout << value << " ";
         msg -> setData(j, value);
     }
@@ -180,44 +165,62 @@ void Leader::sendSchedule()
 {
     int numWorkers = par("numWorkers").intValue();
     int maxScheduleSize = 50;
+    int changekeyPos, reducePos;
 
-    // Definisco l'insieme delle possibili operazioni
-    std::vector<std::string> operations = {"add", "sub", "mul", "div", "lt", "gt", "le", "ge", "changekey", "reduce"};
+    // Define the possible operations set
+    std::vector<std::string> operations = {"add", "sub", "mul", "div", "gt", "lt", "ge", "le", "changekey", "reduce"};
     int op_size = operations.size();
-    std::cout << "Abbiamo " << op_size-1 << " operazioni"<< endl;
+    std::cout << "There are " << op_size << " operations"<< endl;
+
+    for(int i = 0; i < op_size; i++)
+    {
+        if(operations[i] == "changekey")
+        {
+            changekeyPos = i;
+            std::cout << "ChangeKey Pos = "<< changekeyPos << std::endl;
+        }
+        if(operations[i] == "reduce")
+        {
+            reducePos = i;
+            std::cout << "Reduce Pos = "<< reducePos << std::endl;
+        }
+    }
 
     bool reduceFound = false;
 
-    // Genero la dimensione della schedule tra 1 e 15
-    //int scheduleSize = intuniform(1, 100);
-    int scheduleSize = 5;
-    std::cout << "Schedule size: " << scheduleSize << endl;
+    // Generate a random number for the dimension of the schedule
+    int scheduleSize = (rand() % maxScheduleSize) + 1;
+    std::cout << std::endl << "Schedule size: " << scheduleSize << endl;
 
-
-    // Creo un array di stringhe vuoto e un array per i parametri
+    // Instantiate an empty array for the actual schedule and for parameters
     std::vector<std::string> schedule(scheduleSize);
-    schedule = {"add", "mul", "gt", "sub", "reduce"};
     std::vector<int> parameters(scheduleSize);
 
     for(int i = 0; i < scheduleSize; ++i)
     {
-        // Genero un indice casuale per poi trovare l'operazione corrispondente all'indice
-        int randomIndex = intuniform(1, op_size - 1);
+        // Generate a random index to pick a random operation in the set
+        int randomIndex = (rand() % (op_size));
         while(operations[randomIndex] == "reduce" && i != scheduleSize-1)
         {
             reduceFound = true;
-            randomIndex = intuniform(1, op_size) - 1;
-            std::cout << "Found: " << reduceFound << endl;
+            randomIndex = (rand() % (op_size));
+            std::cout << "Reduce found: " << reduceFound << endl;
         }
-        if(randomIndex != 9)
+        if(randomIndex == changekeyPos)
         {
-            // Evito numeri negativi per via della divisione
-            int param = intuniform(1, 10);
+            // Generate a random number between 1 e numWorkers for the changeKey operation
+            int param = 0;
             parameters[i] = param;
-            //schedule[i] = operations[randomIndex];
         }
+            else
+            {
+                // Generate a random number between 1 and 10 avoiding negative numbers due to the division operation
+                int param = (rand() % 10) + 1;
+                parameters[i] = param;
+            }
+        schedule[i] = operations[randomIndex];
     }
-
+    // Sending the schedule and the parameters
     for(int i = 0; i < numWorkers; i++)
     {
         std::cout << "Schedule: ";
@@ -232,7 +235,7 @@ void Leader::sendSchedule()
             msg -> setParameters(j, parameters[j]);
             if(reduceFound == true)
             {
-                schedule[scheduleSize-1] = operations[9];
+                schedule[scheduleSize-1] = operations[reducePos];
                 parameters[scheduleSize-1] = 0;
                 msg -> setSchedule((scheduleSize-1), operations[op_size-1].c_str());
                 msg -> setParameters((scheduleSize-1), 0);
